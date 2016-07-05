4a64e5b02b6893824f2b2ed125ed8b2dce853045
Domingos Junior <djunior_s@hotmail.com>
2016-07-04 22:32:56 -0300

    Creating test add one category and find that

---
+++ library-model/src/test/java/com/library/app/commontest/category/CategoryForTestsRepository.java
@@ -0,0 +1,13 @@
+package com.library.app.commontest.category;
+
+import org.junit.Ignore;
+
+import com.library.app.category.model.Category;
+
+@Ignore
+public class CategoryForTestsRepository {
+	
+	public static Category java() {
+		return new Category("Java");
+	}
+}
