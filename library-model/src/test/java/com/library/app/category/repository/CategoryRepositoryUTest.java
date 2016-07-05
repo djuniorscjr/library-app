9ff16ebb1ccaa80c049aead63180092f20715edc
Domingos Junior <djunior_s@hotmail.com>
2016-07-04 21:11:42 -0300

    Create new module and initial test with category repository

---
+++ library-model/src/test/java/com/library/app/category/repository/CategoryRepositoryUTest.java
@@ -0,0 +1,9 @@
+package com.library.app.category.repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+public class CategoryRepositoryUTest {
+	private EntityManagerFactory emf;
+	private EntityManager em;
+}
